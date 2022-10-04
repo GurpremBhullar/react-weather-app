c5a3a568e2cba1724e9e7f8bae717ed1e578b057
Gurprem Bhullar <gurprembhullar@Gurprems-Air.home>
2022-10-04 18:03:34 -0400

    started using the apis

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '67812815efmsha1dc23a2cc746e5p1b5109jsn8b3789b56309',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo'";
+
+export const WEATHER_API_URL = "28e8f3efb740c29dfea71effd97c7ec1";
+
+export const WEATHER_API_KEY ="28e8f3efb740c29dfea71effd97c7ec1";
+
